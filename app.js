11c486207d1121ed0d039a01565dfd6326ef6558
Syed Muhammad Aizaz Ali <136581038+syedMuhammadAizazALi@users.noreply.github.com>
2023-08-22 03:00:45 -0700

    array

---
+++ app.js
@@ -0,0 +1,84 @@
+// let student =  [10, 20 ,30 ,40]
+// console.log(student);
+
+// let fruits = ['grapes', 'apple', 'watermelon', 'mango', 'peach', 'nashpati'];
+// fruits.push('hi')
+// fruits.pop()
+// fruits.unshift('hello')
+// fruits.shift ();
+// fruits.splice(1,4 ,'ok')
+// console.log(fruits);
+
+// let eaten = fruits.slice(2,5)
+// console.log(eaten);
+
+
+
+// let names= 'my name is aizaz ok bhai ok '
+// // console.log(names);
+// let add = names.split(' ');
+// console.log(add);
+
+
+
+
+// let student = 'my aizaz ali  good student ho ok'
+// let newarra = student.split(' ')
+// newarra[2] = 'Raza'
+// console.log(newarra);
+// let newst= newarra.join( ' ' )
+// console.log(newst);
+
+
+// let num1= [1,2,3,4]
+// let num2 =[ 5,6,7,8]
+// let number = num1.concat(num2)
+// number.reverse()
+// console.log(number);
+
+// let num1 =[9,8,7,6]
+// // num1.sort()
+// // console.log(num1);
+// let num2 = num1.indexOf(100)
+// console.log(num2);
+
+
+
+// assignmentdone
+
+let student =[ 'Alice', 'Bob', 'Charlie', 'David', 'Eve']
+student.unshift('Frank', 'Charlie')
+student.splice(5,6)
+student.push('Grace','Helen')
+student[3] = 'hi'
+student[6] = 'pk'
+console.log(student);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
